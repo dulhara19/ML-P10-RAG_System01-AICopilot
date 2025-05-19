06e358b74d433cded76aa55a44e21cfe20ddd00e
dulhara <wkldulhara@gmail.com>
2025-05-19 22:48:01 +0530

    updated fe

---
+++ front-end/tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 This is key for React (Vite)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
